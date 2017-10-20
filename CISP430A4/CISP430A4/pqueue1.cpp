b4578f5b88bd497985c63c00e537bf22dee232b0
NathanYarrison <31556575+NathanYarrison@users.noreply.github.com>
2017-10-19 21:58:40 -0700

    Updated insert() and get_front(). Having trouble deleting the data in get_front() though.

--- CISP430A4/CISP430A4/pqueue1.cpp
+++ CISP430A4/CISP430A4/pqueue1.cpp
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <cassert>
+#include <iostream>
 #include "pqueue1.h"
+
+using namespace std;
 
 typedef int Item;
 
@@ -18,35 +21,74 @@ PriorityQueue::PriorityQueue(const PriorityQueue & source)
 
 PriorityQueue::~PriorityQueue()
 {
+	while (head_ptr != NULL)
+	{
+		Node *temp = head_ptr;
+		head_ptr = head_ptr->link;
+		delete temp;
+	}
 	many_nodes = 0;
 }
 
 void PriorityQueue::operator=(const PriorityQueue & source)
 {
+	//Node *temp;
+	while (source.head_ptr != NULL)
+	{
+		head_ptr = source.head_ptr->link;
+	}
+
+	many_nodes = source.many_nodes;
+
 }
 
 void PriorityQueue::insert(const Item & entry, unsigned int priority)
 {
-	Node* n; // Pointer to a specific node in linked list.
-	n->priority = priority;
-	if (head_ptr == NULL) // Checks if head_ptr is equal to NULL.
+	// Inserts a a copy of entry into the queue at the designated priority.
+	// If there is no items in the queue, then entry is inserted at the front of the queue.
+	// If there is already an existing item with the same priority in the queue, then the new entry is inserted behind that item.
+
+	Node *temp, *q; // Creates pointers to hold temporary versions of the given entry and priority for use in function.
+	temp = new Node;
+	temp->data = entry;
+	temp->priority = priority;
+
+	if (head_ptr == NULL || priority > head_ptr->priority) // Checks if head_ptr points to NULL or if given priority is less than the priority of the head_ptr item.
 	{
-		// If it is, then there are no items in the queue and the new entry must be inserted at the front but keep priority.
-		head_ptr = n;
+		// If head_ptr points to NULL, then there are no items in the queue.
+		// Or, if the given priority is greater than the priority of the head_ptr item, then... 
+		temp->link = head_ptr; // Assign address pointed to by head_ptr to the temp link pointer.
+		head_ptr = temp; // Then assign value stored in temp to head_ptr to ensure the head_ptr points at the front value.
 	}
-	else // Else, there is an item in the queue and priority must be checked, greater priority is first in queue.
+	else // Else, there are items in the queue and the new entry must be inserted at the appropriate priority location.
 	{
-		Node* temp = head_ptr;
-		if (n->priority > temp->priority)
+		q = head_ptr;
+		while (q->link != NULL && q->link->priority >= priority)
 		{
-			head_ptr->link = n;
+			q = q->link;
 		}
-		else
+		temp->link = q->link;
+		q->link = temp;
 	}
+
+	++many_nodes;
 }
 
 Item PriorityQueue::get_front()
 {
-	assert(!size() < 0);
-	return head_ptr->data;
+	// Gets the item at the front of the queue (the item with the highest priority), returns it to call, and removes it from the queue.
+
+	// Assert that the size is greater than 0.
+	assert(size() > 0);
+
+	// Determine which item has the highest priority.
+	// If multiple items have the same priority, then the first item in is returned.
+	Item info;
+
+	info = head_ptr->data; // head_ptr should be pointing to the first item in the queue (either the item with the highest priority or the first entered into queue.
+						  // Assign this to temp2 for manipulation.
+	delete head_ptr->data;
+	head_ptr = head_ptr->link; // Points head_ptr in the next item in the queue (either the item with the next highest priority or the next item inserted).
+	--many_nodes;
+	return info; // returns value to function call.
 }
